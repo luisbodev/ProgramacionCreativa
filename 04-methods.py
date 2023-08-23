ec3881790a0be53d42d757f06b19573516a7efc3
Luis Bonilla <luisbodev@gmail.com>
2023-08-22 21:12:37 -0600

    Add methods exercise

---
+++ 04-methods.py
@@ -0,0 +1,13 @@
+text = input("Ingrese un texto: ")
+
+texto_en_mayusculas = text.upper()
+print("El texto que has ingresado en mayúsculas es: ")
+print(texto_en_mayusculas)
+
+
+url = input("Ingrese una url: ")
+url_modificado = url.removeprefix('www.')
+
+print('El url modificado es: ')
+print(url_modificado)
+
